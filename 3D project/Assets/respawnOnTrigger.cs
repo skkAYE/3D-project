c510e346a23c08bbed11320f44f2b58aedca8f57
arnaud <skaillexsoop@gmail.com>
2019-03-01 13:45:26 +0100

    new version

---
+++ 3D project/Assets/respawnOnTrigger.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class respawnOnTrigger : MonoBehaviour
+{
+    [SerializeField] private Transform player;
+    [SerializeField] private Transform respawnPoint;
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            player.transform.position = respawnPoint.position;
+        }
+
+
+    }
+}
