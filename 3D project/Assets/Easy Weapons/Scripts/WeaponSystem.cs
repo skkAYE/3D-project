c3d37ea410e98345254b3229237af49af2f52bac
arnaud <skaillexsoop@gmail.com>
2019-03-02 21:56:14 +0100

    destruct wall + import

--- 3D project/Assets/Easy Weapons/Scripts/WeaponSystem.cs
+++ 3D project/Assets/Easy Weapons/Scripts/WeaponSystem.cs
@@ -13,9 +13,6 @@ public class WeaponSystem : MonoBehaviour
 	public GameObject[] weapons;				// The array that holds all the weapons that the player has
 	public int startingWeaponIndex = 0;			// The weapon index that the player will start with
 	private int weaponIndex;					// The current index of the active weapon
-    public GameObject leftArm;
-    public GameObject rightArm;
-    public GameObject metarig;
 
     // Use this for initialization
     void Start()
